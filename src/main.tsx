d041d9eeac51580403e35f9ba9018cd6eaa4f5d2
Jeffrey Jernstrom <jeffrey.jernstrom@gmail.com>
2024-11-19 15:04:48 -0500

    Run prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 //import './index.css'
-import { Home } from './pages/Home';
+import { Home } from "./pages/Home";
 //import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home/>
+    <Home />
   </StrictMode>,
-)
+);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
-import { Container, Stack, Typography } from "@mui/material"
-import EagleLogo from '../../assets/EagleLogo.png'
-import DanWelding from '../../assets/DanWelding.jpg'
+import { Container, Stack, Typography } from "@mui/material";
+import EagleLogo from "../../assets/EagleLogo.png";
+import DanWelding from "../../assets/DanWelding.jpg";
 
 export const Home = () => {
   return (
@@ -9,49 +9,86 @@ export const Home = () => {
         <title>Eagle Welding</title>
       </head>
       <body>
-      <Container>
-      <Stack gap={2} justifyContent='center' display='flex'>
-
-        <Stack 
-          alignItems='center' 
-          direction='row' 
-          gap={4} 
-          margin='auto' 
-          marginTop={4} 
-          marginBottom={4} 
-          flexWrap='wrap' >
-          <img src={EagleLogo} alt='Eagle Welding Logo' height={100} width={300}/>
-          <Stack>
-          <Typography variant="h4">Where Metal Meets Craftsmanship</Typography>
-          <Typography variant="h6"><em>Your Local Mobile Welding Service, Monroe PA</em></Typography>
-          <Typography variant="h6">
-            <strong>Call: 570-212-9595  |  Email: dan@eaglewelds.com</strong>
-          </Typography>
-          </Stack>
-        </Stack>
-
-        <Stack direction='row' justifyContent='center' alignItems='center' flexWrap='wrap' margin='auto'>
-        <Stack marginRight={12}>
-          <Typography variant="h4" maxWidth={500} marginBottom={2}>Mobile Welding Repairs and Custom Fabrication</Typography>
-          <Typography maxWidth={600} marginBottom={2}>
-          Eagle Welding and Fabrication LLC is a local, family-owned business dedicated to quality welding and fabrication. With over ten years of experience, Dan has honed his skills in welding and metalwork, bringing a lifetime of local knowledge from Monroe County. His commitment to precision and a strong background in automotive and custom metalwork make him the ideal choice for any project. Specializing in mobile welding, we bring our services directly to you, wherever your project is located.
-          </Typography>
-          <Typography maxWidth={600} marginBottom={2}>
-          Whether you're creating an art piece, need a custom bear box, or require assistance with automotive repairs, we have the skills and expertise to bring your vision to life.
-          </Typography>
-          <Typography maxWidth={600} marginBottom={2}>
-          <strong>Contact us today for an estaimate on your project!</strong>
-          </Typography>
-          <Typography maxWidth={600}>
-          <strong>Call: 570-212-9595  |  Email: dan@eaglewelds.com</strong>
-          </Typography>
+        <Container>
+          <Stack gap={2} justifyContent="center" display="flex">
+            <Stack
+              alignItems="center"
+              direction="row"
+              gap={4}
+              margin="auto"
+              marginTop={4}
+              marginBottom={4}
+              flexWrap="wrap"
+            >
+              <img
+                src={EagleLogo}
+                alt="Eagle Welding Logo"
+                height={100}
+                width={300}
+              />
+              <Stack>
+                <Typography variant="h4">
+                  Where Metal Meets Craftsmanship
+                </Typography>
+                <Typography variant="h6">
+                  <em>Your Local Mobile Welding Service, Monroe PA</em>
+                </Typography>
+                <Typography variant="h6">
+                  <strong>
+                    Call: 570-212-9595 | Email: dan@eaglewelds.com
+                  </strong>
+                </Typography>
+              </Stack>
             </Stack>
-          <img src={DanWelding} alt='Dan grinding a part for a project' height={400} width={300} />
-        </Stack>
 
-        </Stack>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              flexWrap="wrap"
+              margin="auto"
+            >
+              <Stack marginRight={12}>
+                <Typography variant="h4" maxWidth={500} marginBottom={2}>
+                  Mobile Welding Repairs and Custom Fabrication
+                </Typography>
+                <Typography maxWidth={600} marginBottom={2}>
+                  Eagle Welding and Fabrication LLC is a local, family-owned
+                  business dedicated to quality welding and fabrication. With
+                  over ten years of experience, Dan has honed his skills in
+                  welding and metalwork, bringing a lifetime of local knowledge
+                  from Monroe County. His commitment to precision and a strong
+                  background in automotive and custom metalwork make him the
+                  ideal choice for any project. Specializing in mobile welding,
+                  we bring our services directly to you, wherever your project
+                  is located.
+                </Typography>
+                <Typography maxWidth={600} marginBottom={2}>
+                  Whether you're creating an art piece, need a custom bear box,
+                  or require assistance with automotive repairs, we have the
+                  skills and expertise to bring your vision to life.
+                </Typography>
+                <Typography maxWidth={600} marginBottom={2}>
+                  <strong>
+                    Contact us today for an estaimate on your project!
+                  </strong>
+                </Typography>
+                <Typography maxWidth={600}>
+                  <strong>
+                    Call: 570-212-9595 | Email: dan@eaglewelds.com
+                  </strong>
+                </Typography>
+              </Stack>
+              <img
+                src={DanWelding}
+                alt="Dan grinding a part for a project"
+                height={400}
+                width={300}
+              />
+            </Stack>
+          </Stack>
         </Container>
       </body>
     </>
   );
-}
+};
