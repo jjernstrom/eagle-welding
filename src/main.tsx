04477a6515ebe537daa90db6a928a2cc7fe35823
Jeffrey Jernstrom <jeffrey.jernstrom@gmail.com>
2025-01-08 15:35:28 -0500

    Clean up and update main imports to use base css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-//import './index.css'
 import { Home } from "./pages/Home";
-//import App from './App.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
