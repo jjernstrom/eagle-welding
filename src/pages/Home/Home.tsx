de594db73b08383598dbdd0b128f49a3991b5283
Jeffrey Jernstrom <jeffrey.jernstrom@gmail.com>
2025-01-30 16:24:42 -0500

    Add new components to home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,37 +1,13 @@
-import { Stack, Typography } from "@mui/material";
-import EagleLogo from "@/assets/EagleLogo.png";
-import { Hero } from "@/components";
+import { Stack } from "@mui/material";
+import { Footer, Hero, NavBar, Services } from "@/components";
 
 export const Home = () => {
   return (
-    <Stack gap={2} justifyContent="center" display="flex">
-      <Stack
-        alignItems="center"
-        direction="row"
-        gap={4}
-        margin="auto"
-        marginTop={4}
-        marginBottom={4}
-        flexWrap="wrap"
-      >
-        <img
-          src={EagleLogo}
-          alt="Eagle Welding Logo"
-          height={100}
-          width={300}
-        />
-        <Stack>
-          <Typography variant="h4">Where Metal Meets Craftsmanship</Typography>
-          <Typography variant="h6">
-            <em>Your Local Mobile Welding Service, Monroe PA</em>
-          </Typography>
-          <Typography variant="h6">
-            <strong>Call: 570-212-9595 | Email: dan@eaglewelds.com</strong>
-          </Typography>
-        </Stack>
-      </Stack>
-
+    <Stack>
+      <NavBar />
       <Hero />
+      <Services />
+      <Footer />
     </Stack>
   );
 };
